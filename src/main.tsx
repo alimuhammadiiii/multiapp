c2b75fb12113cb282a4c80d8c5b0f579f85c1970
Ali <Alimuhammadiii@outlook.com>
2025-05-11 04:31:19 +0330

    adjust multiApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
+const queryClient = new QueryClient();
+
+// Create a new router instance
 const router = createRouter({ routeTree });
 
+// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
@@ -13,13 +19,14 @@ declare module "@tanstack/react-router" {
 }
 
 // Render the app
-
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { FcTodoList } from "react-icons/fc";
+import { SiTheweatherchannel } from "react-icons/si";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -6,8 +8,37 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="flex justify-center items-center h-full bg-[url(assets/bg-home.svg)] bg-contain">
+      <div className="p-2 flex gap-2 flex-wrap h-64 w-64">
+        <Link
+          to="/"
+          className=" h-28 w-28 rounded  bg-[#f4f0f0]  flex items-center justify-center flex-col text-black"
+        >
+          <SiTheweatherchannel size={80} />
+          coming soon..
+        </Link>{" "}
+        <Link
+          to="/about"
+          className="[&.active]:font-bold h-28 w-28 rounded bg-[#f4f0f0] flex items-center justify-center"
+        >
+          <p className="font-bold">AboutUs</p>
+        </Link>
+        <Link
+          to="/todoApp"
+          className="font-bold h-28 w-28 rounded bg-[#f4f0f0] [&.active]:text-[#535bf2] flex items-center justify-center flex-col"
+        >
+          <FcTodoList size={80} />
+          TodoApp
+        </Link>
+        <Link
+          to="/"
+          className=" h-28 w-28 rounded  flex-col bg-white flex items-center justify-center"
+        >
+          <div className="w-23 h-23 bg-[url(assets/Tic-Tac-Toe.jpg)] bg-contain bg-no-repeat"></div>
+          coming soon..
+        </Link>
+      </div>
+      <hr />
     </div>
   );
 }
